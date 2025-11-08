4fb57aff625c403ef19cf169586ed4b4468767c5
John <101830286+johnpradoo@users.noreply.github.com>
2025-11-08 18:34:10 -0500

    Update index.js

--- index.js
+++ index.js
@@ -115,6 +115,16 @@ const addonInterface = builder.getInterface();
 
 http
   .createServer((req, res) => {
+    // Cabeceras CORS
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    if (req.method === "OPTIONS") {
+      res.writeHead(204);
+      res.end();
+      return;
+    }
+
     if (req.url === "/manifest.json") {
       res.writeHead(200, { "Content-Type": "application/json" });
       res.end(JSON.stringify(addonInterface.manifest));
